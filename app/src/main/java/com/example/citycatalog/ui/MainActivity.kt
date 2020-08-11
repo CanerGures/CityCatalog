0b80a7610e6ccf8d6b277ee5b359ae0bf66be0a2
Yusuf Caner Güreş <gurescaner@gmail.com>
2020-08-11 17:01:13 +0300

    First Commit with excitement!

---
+++ app/src/main/java/com/example/citycatalog/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.citycatalog.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.citycatalog.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/citycatalog/util/AppUtils.kt
@@ -0,0 +1,8 @@
+package com.example.citycatalog.util
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+
+infix fun <T : AppCompatActivity> AppCompatActivity.extStartActivity(className: Class<T>) {
+    startActivity(Intent(this, className))
+}
