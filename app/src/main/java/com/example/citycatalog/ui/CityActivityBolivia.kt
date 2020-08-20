8c6136393eb8cc55b4c66c8e25f42bde6dd03e1a
Yusuf Caner Güreş <gurescaner@gmail.com>
2020-08-20 14:35:00 +0300

    Some adjustment and refactoring done.

--- app/src/main/java/com/example/citycatalog/ui/CityActivityBolivia.kt
+++ app/src/main/java/com/example/citycatalog/ui/CityActivityBolivia.kt
@@ -39,55 +39,9 @@ class CityActivityBolivia : AppCompatActivity() {
 
         var isOpen = false
 
-        val list = ArrayList<TravelLocationModel>()
 
-        val card11 = TravelLocationModel(
-            "Bolivia",
-            "Salar de Uyuni",
-            "https://daily.jstor.org/wp-content/uploads/2015/08/Solar_de_Uyuni_1050x700.jpg",
-            4.8f,
-            11,
-            "Salar de Uyuni (or Salar de Tunupa) is the world's largest salt flat, or playa, at over 10,000 square kilometres (3,900 sq mi) in area. It is in the Daniel Campos Province in Potosí in southwest Bolivia, near the crest of the Andes at an elevation of 3,656 meters (11,995 ft) above sea level. The Salar was formed as a result of transformations between several prehistoric lakes. It is covered by a few meters of salt crust, which has an extraordinary flatness with the average elevation variations within one meter over the entire area of the Salar. The crust serves as a source of salt and covers a pool of brine, which is exceptionally rich in lithium. The large area, clear skies, and exceptional flatness of the surface make the Salar ideal for calibrating the altimeters of Earth observation satellites"
-        )
-        val card12 = TravelLocationModel(
-            "Bolivia",
-            "Sajama National Park",
-            "https://img.locationscout.net/images/2019-04/sajama-national-park-bolivia_l.jpeg",
-            4.5f,
-            12,
-            "Sajama National Park is a national park located in the Oruro Department, Bolivia. It borders Lauca National Park in Chile. The park is home to indigenous people, known as the Aymara, whose influential ancient culture can be seen in various aspects throughout the park. The park contains unique cultural artifacts and ecological wonders, making it an exemplary location for ecotourism. Many different indigenous plants and animals are exclusive to this area; therefore, its continued conservation is of great ecological importance. Management of the park operates under a co-administrative approach, with local people and park conservationists engaging in a constant dialogue regarding park upkeep and policy."
-        )
-        val card13 = TravelLocationModel(
-            "Bolivia",
-            "La Glorieta Castle, Sucre",
-            "https://media-cdn.tripadvisor.com/media/photo-s/0b/48/2c/fb/torres.jpg",
-            4.9f,
-            13,
-            "See the eclectic European-inspired architecture of La Glorieta Castle, the former home of Don Francisco de Argandona and his wife Clotilde. The couple returned from a tour of the Continent and commissioned a castle that combines elements of the designs they saw there. Architect Antonio Camponovo mixed Gothic, Baroque, Rococo, Byzantine, and Manierista in this 19th-century structure. The reddish-pinkish hue of the building lends the place a fairy-tale feel. Note that the decorative interiors are fairly empty of furniture, so you'll need to use your imagination. Stroll the open (if unkempt) grounds, and climb one of the castle's towers for views of the nearby military base."
-        )
-        val card14 = TravelLocationModel(
-            "Bolivia",
-            "Death Road, La Paz",
-            "https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2019/10/best-places-to-visit-in-Bolivia-Matthew-Straubmuller-Death-Road.jpg",
-            4.4f,
-            14,
-            "The Yungas Road is a cycle route about 60 km long which links the city of La Paz and the Yungas region of Bolivia. It draws about 25,000 tourists per year and is a major La Paz tourist attraction.[2] Many tour operators cater to downhill mountain biking, providing information, guides, transport and equipment. At least 18 cyclists have died on the road since 1998. The tourist route is a 64 km long road with 3500 metres of descent. The route includes the Cotapata-Santa Bárbara section. It replaced the old road, built in 1930. It was considered dangerous because of its steep slopes, narrow single track, lack of guardrails, rain, and fog, and was nicknamed the \"Road of Death\"."
-        )
-        val card15 = TravelLocationModel(
-            "Bolivia",
-            "Mount Illimani, La Paz",
-            "https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2019/10/best-places-to-visit-in-Bolivia-@akhamani-llimani.jpg",
-            4.6f,
-            15,
-            "Illimani (Aymara) is the highest mountain in the Cordillera Real of western Bolivia. It lies near the cities of El Alto and La Paz at the eastern edge of the Altiplano. It is the second highest peak in Bolivia, after Nevado Sajama, and the eighteenth highest peak in South America. The snow line lies at about 4,570 metres (15,000 ft) above sea level, and glaciers are found on the northern face at 4,983 m (16,350 ft). The mountain has four main peaks; the highest is the south summit, Nevado Illimani, which is a popular ascent for mountain climbers. Geologically, Illimani is composed primarily of granodiorite, intruded during the Cenozoic era into the sedimentary rock, which forms the bulk of the Cordillera Real."
-        )
-        list.add(card11)
-        list.add(card12)
-        list.add(card13)
-        list.add(card14)
-        list.add(card15)
-
-        viewPager.adapter = ViewPagerAdapterCities(list) { data: TravelLocationModel ->
+        val mockData = createMockData()
+        viewPager.adapter = ViewPagerAdapterCities(mockData) { data: TravelLocationModel ->
             itemClicked(
                 data
             )
@@ -147,6 +101,58 @@ class CityActivityBolivia : AppCompatActivity() {
             val intent = Intent(it.context, CityActivityBolivia::class.java)
             it.context.startActivity(intent)
         }
+    }
+
+    private fun createMockData(): List<TravelLocationModel> {
+        val list = ArrayList<TravelLocationModel>()
+
+        val card11 = TravelLocationModel(
+            "Bolivia",
+            "Salar de Uyuni",
+            "https://daily.jstor.org/wp-content/uploads/2015/08/Solar_de_Uyuni_1050x700.jpg",
+            4.8f,
+            11,
+            "Salar de Uyuni (or Salar de Tunupa) is the world's largest salt flat, or playa, at over 10,000 square kilometres (3,900 sq mi) in area. It is in the Daniel Campos Province in Potosí in southwest Bolivia, near the crest of the Andes at an elevation of 3,656 meters (11,995 ft) above sea level. The Salar was formed as a result of transformations between several prehistoric lakes. It is covered by a few meters of salt crust, which has an extraordinary flatness with the average elevation variations within one meter over the entire area of the Salar. The crust serves as a source of salt and covers a pool of brine, which is exceptionally rich in lithium. The large area, clear skies, and exceptional flatness of the surface make the Salar ideal for calibrating the altimeters of Earth observation satellites"
+        )
+        val card12 = TravelLocationModel(
+            "Bolivia",
+            "Sajama National Park",
+            "https://img.locationscout.net/images/2019-04/sajama-national-park-bolivia_l.jpeg",
+            4.5f,
+            12,
+            "Sajama National Park is a national park located in the Oruro Department, Bolivia. It borders Lauca National Park in Chile. The park is home to indigenous people, known as the Aymara, whose influential ancient culture can be seen in various aspects throughout the park. The park contains unique cultural artifacts and ecological wonders, making it an exemplary location for ecotourism. Many different indigenous plants and animals are exclusive to this area; therefore, its continued conservation is of great ecological importance. Management of the park operates under a co-administrative approach, with local people and park conservationists engaging in a constant dialogue regarding park upkeep and policy."
+        )
+        val card13 = TravelLocationModel(
+            "Bolivia",
+            "La Glorieta Castle, Sucre",
+            "https://media-cdn.tripadvisor.com/media/photo-s/0b/48/2c/fb/torres.jpg",
+            4.9f,
+            13,
+            "See the eclectic European-inspired architecture of La Glorieta Castle, the former home of Don Francisco de Argandona and his wife Clotilde. The couple returned from a tour of the Continent and commissioned a castle that combines elements of the designs they saw there. Architect Antonio Camponovo mixed Gothic, Baroque, Rococo, Byzantine, and Manierista in this 19th-century structure. The reddish-pinkish hue of the building lends the place a fairy-tale feel. Note that the decorative interiors are fairly empty of furniture, so you'll need to use your imagination. Stroll the open (if unkempt) grounds, and climb one of the castle's towers for views of the nearby military base."
+        )
+        val card14 = TravelLocationModel(
+            "Bolivia",
+            "Death Road, La Paz",
+            "https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2019/10/best-places-to-visit-in-Bolivia-Matthew-Straubmuller-Death-Road.jpg",
+            4.4f,
+            14,
+            "The Yungas Road is a cycle route about 60 km long which links the city of La Paz and the Yungas region of Bolivia. It draws about 25,000 tourists per year and is a major La Paz tourist attraction.[2] Many tour operators cater to downhill mountain biking, providing information, guides, transport and equipment. At least 18 cyclists have died on the road since 1998. The tourist route is a 64 km long road with 3500 metres of descent. The route includes the Cotapata-Santa Bárbara section. It replaced the old road, built in 1930. It was considered dangerous because of its steep slopes, narrow single track, lack of guardrails, rain, and fog, and was nicknamed the \"Road of Death\"."
+        )
+        val card15 = TravelLocationModel(
+            "Bolivia",
+            "Mount Illimani, La Paz",
+            "https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2019/10/best-places-to-visit-in-Bolivia-@akhamani-llimani.jpg",
+            4.6f,
+            15,
+            "Illimani (Aymara) is the highest mountain in the Cordillera Real of western Bolivia. It lies near the cities of El Alto and La Paz at the eastern edge of the Altiplano. It is the second highest peak in Bolivia, after Nevado Sajama, and the eighteenth highest peak in South America. The snow line lies at about 4,570 metres (15,000 ft) above sea level, and glaciers are found on the northern face at 4,983 m (16,350 ft). The mountain has four main peaks; the highest is the south summit, Nevado Illimani, which is a popular ascent for mountain climbers. Geologically, Illimani is composed primarily of granodiorite, intruded during the Cenozoic era into the sedimentary rock, which forms the bulk of the Cordillera Real."
+        )
+        list.add(card11)
+        list.add(card12)
+        list.add(card13)
+        list.add(card14)
+        list.add(card15)
+        return list
+
     }
 
     private fun itemClicked(data: TravelLocationModel) {
--- app/src/main/java/com/example/citycatalog/ui/CityActivityIstanbul.kt
+++ app/src/main/java/com/example/citycatalog/ui/CityActivityIstanbul.kt
@@ -43,55 +43,9 @@ class CityActivityIstanbul : AppCompatActivity() {
 
         var isOpen = false
 
-        val list = ArrayList<TravelLocationModel>()
 
-        val card11 = TravelLocationModel(
-            "Istanbul",
-            "Hagia Sophia",
-            "https://i.hurimg.com/i/hdn/75/0x0/5c0d246dc03c0e15a49c546a.jpg",
-            4.8f,
-            16,
-            "The Hagia Sophia Grand Mosque / Ayasofya-i Kebir Cami-i Şerifi, with its innovative architecture, rich history, religious significance and extraordinary characteristics has been fighting against time for centuries, is the largest Eastern Roman Church in Istanbul. Constructed three times in the same location, it is the world’s oldest and fastest-completed cathedral. With its breathtaking domes that look like hanging in the air, monolithic marble columns and unparalleled mosaics, is one of the wonders of world’s architecture history."
-        )
-        val card12 = TravelLocationModel(
-            "Istanbul",
-            "Grand Bazaar",
-            "https://media.timeout.com/images/105265906/630/472/image.jpg",
-            4.5f,
-            17,
-            "One of the largest and oldest covered bazaars in the world, the Grand Bazaar is 30,700 square meters with over 60 streets and alleys and 4,000 shops. The original historical core of the bazaar, İç Bedesten, was completed by Mehmet the Conqueror in 1461. A “bedesten” refers to an indoor arcade with shops and there are several areas within the bazaar referred to by this name. Over the years, the Grand Bazaar expanded from this core of two bedestens to become a sprawling roofed complex of thousands of shops, fringed by the tradesmen’s inns and workshops known as hans. According to the Ottoman traveler Evliya Çelebi’s Seyahatname, by the seventeenth century the Kapalı Çarşı (or the Çarşı-yı-Kebir as it was known at the time) had reached its present size, with over 4,000 shops and nearly 500 stalls known in Turkish as dolap (literally translated to “cupboard”)."
-        )
-        val card13 = TravelLocationModel(
-            "Istanbul",
-            "St. Antoine Church",
-            "https://i.pinimg.com/originals/f9/bf/18/f9bf18bcd94b6f8c75b75df762a5e253.jpg",
-            4.9f,
-            18,
-            "An imposing red brick building, it was originally built on this site in 1725, but was demolished in the early 20th century,in order to make way for a tramway. However the church was rebuilt by the architect, Giuliu Mongeri and completed in 1912, and stands as a fine example of the Italian neo-Gothic and Tuscan-Lombard styles."
-        )
-        val card14 = TravelLocationModel(
-            "Istanbul",
-            "Dolmabahce Palace",
-            "https://toursce.com/wp-content/uploads/2018/08/Dolmabahce-palace-museum-1.jpg",
-            4.4f,
-            19,
-            "Dolmabahce Palace built in 19 th century is one of the most glamorous palaces in the world. It was the administrative center of the late Ottoman Empire with the last of Ottoman Sultans was residing there. After the foundation of the Turkish Republic in Ankara, Mustafa Kemal Ataturk transferred all government functions to the youthful capital but on his visits to Istanbul Ataturk occupied only a small room at Dolmabahce Palace as his own."
-        )
-        val card15 = TravelLocationModel(
-            "Istanbul",
-            "Galata Tower",
-            "https://uted.org/galeri/20197194238.jpg",
-            4.6f,
-            20,
-            "The Galata Tower, Galata Kulesi in Turkish, is one of the highest and oldest towers of Istanbul. 63 meter (206 feet) high tower provides a panoramic view of the old town. It was built in the 14th century by the Genoese colony as part of the defense wall surrounding their district at Galata directly opposite ancient Constantinopolis. They called the tower as \"Christea Turris\", or \"Tower of Christ\"."
-        )
-        list.add(card11)
-        list.add(card12)
-        list.add(card13)
-        list.add(card14)
-        list.add(card15)
-
-        viewPager.adapter = ViewPagerAdapterCities(list) { data: TravelLocationModel ->
+        val mockList = createMockData()
+        viewPager.adapter = ViewPagerAdapterCities(mockList) { data: TravelLocationModel ->
             itemClicked(
                 data
             )
@@ -162,5 +116,55 @@ class CityActivityIstanbul : AppCompatActivity() {
         startActivity(intent)
     }
 
+    private fun createMockData(): List<TravelLocationModel> {
+        val list = ArrayList<TravelLocationModel>()
 
+        val card11 = TravelLocationModel(
+            "Istanbul",
+            "Hagia Sophia",
+            "https://i.hurimg.com/i/hdn/75/0x0/5c0d246dc03c0e15a49c546a.jpg",
+            4.8f,
+            16,
+            "The Hagia Sophia Grand Mosque / Ayasofya-i Kebir Cami-i Şerifi, with its innovative architecture, rich history, religious significance and extraordinary characteristics has been fighting against time for centuries, is the largest Eastern Roman Church in Istanbul. Constructed three times in the same location, it is the world’s oldest and fastest-completed cathedral. With its breathtaking domes that look like hanging in the air, monolithic marble columns and unparalleled mosaics, is one of the wonders of world’s architecture history."
+        )
+        val card12 = TravelLocationModel(
+            "Istanbul",
+            "Grand Bazaar",
+            "https://media.timeout.com/images/105265906/630/472/image.jpg",
+            4.5f,
+            17,
+            "One of the largest and oldest covered bazaars in the world, the Grand Bazaar is 30,700 square meters with over 60 streets and alleys and 4,000 shops. The original historical core of the bazaar, İç Bedesten, was completed by Mehmet the Conqueror in 1461. A “bedesten” refers to an indoor arcade with shops and there are several areas within the bazaar referred to by this name. Over the years, the Grand Bazaar expanded from this core of two bedestens to become a sprawling roofed complex of thousands of shops, fringed by the tradesmen’s inns and workshops known as hans. According to the Ottoman traveler Evliya Çelebi’s Seyahatname, by the seventeenth century the Kapalı Çarşı (or the Çarşı-yı-Kebir as it was known at the time) had reached its present size, with over 4,000 shops and nearly 500 stalls known in Turkish as dolap (literally translated to “cupboard”)."
+        )
+        val card13 = TravelLocationModel(
+            "Istanbul",
+            "St. Antoine Church",
+            "https://i.pinimg.com/originals/f9/bf/18/f9bf18bcd94b6f8c75b75df762a5e253.jpg",
+            4.9f,
+            18,
+            "An imposing red brick building, it was originally built on this site in 1725, but was demolished in the early 20th century,in order to make way for a tramway. However the church was rebuilt by the architect, Giuliu Mongeri and completed in 1912, and stands as a fine example of the Italian neo-Gothic and Tuscan-Lombard styles."
+        )
+        val card14 = TravelLocationModel(
+            "Istanbul",
+            "Dolmabahce Palace",
+            "https://toursce.com/wp-content/uploads/2018/08/Dolmabahce-palace-museum-1.jpg",
+            4.4f,
+            19,
+            "Dolmabahce Palace built in 19 th century is one of the most glamorous palaces in the world. It was the administrative center of the late Ottoman Empire with the last of Ottoman Sultans was residing there. After the foundation of the Turkish Republic in Ankara, Mustafa Kemal Ataturk transferred all government functions to the youthful capital but on his visits to Istanbul Ataturk occupied only a small room at Dolmabahce Palace as his own."
+        )
+        val card15 = TravelLocationModel(
+            "Istanbul",
+            "Galata Tower",
+            "https://uted.org/galeri/20197194238.jpg",
+            4.6f,
+            20,
+            "The Galata Tower, Galata Kulesi in Turkish, is one of the highest and oldest towers of Istanbul. 63 meter (206 feet) high tower provides a panoramic view of the old town. It was built in the 14th century by the Genoese colony as part of the defense wall surrounding their district at Galata directly opposite ancient Constantinopolis. They called the tower as \"Christea Turris\", or \"Tower of Christ\"."
+        )
+        list.add(card11)
+        list.add(card12)
+        list.add(card13)
+        list.add(card14)
+        list.add(card15)
+
+        return list
+    }
 }
